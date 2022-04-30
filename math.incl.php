b3a4aa7f13d164a52c7aa9c7b34f21ea2d672ce6
Nestor Barone <jesuscaesar@outlook.com>
2022-04-30 10:50:22 +0000

    Add

---
+++ math.incl.php
@@ -0,0 +1,5 @@
+<script src="math.js?rev=<?=time();?>"></script>
+<script src="nerdamer.js?rev=<?=time();?>"></script>
+<script src="algebra.js?rev=<?=time();?>"></script>
+<script src="calculus.js?rev=<?=time();?>"></script>
+<script src="solve.js?rev=<?=time();?>"></script>
